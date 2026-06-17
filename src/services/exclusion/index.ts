8af78625c543c343edeb8e643fd44ca92385110f
hamit-gulhan <hamit@proxai.co>
2026-06-17 15:46:30 +0300

    feat(exclusion): barrel export

---
+++ src/services/exclusion/index.ts
@@ -0,0 +1,3 @@
+// src/services/exclusion/index.ts
+export { isProjectExcluded, normalizeFolderPath } from 'services/exclusion/match.ts';
+export { loadExcludedProjects, EXCLUDED_PROJECTS_FILE_NAME } from 'services/exclusion/load.ts';
